92ee3547f70ed708981c26f782b032d99d1e1af8
ethan-jian <729381317@qq.com>
2021-05-22 17:15:03 +0800

    数据库配置为mysql

--- ApiAutoTestManage/settings.py
+++ ApiAutoTestManage/settings.py
@@ -112,12 +112,24 @@ WSGI_APPLICATION = 'ApiAutoTestManage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '20110106',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
